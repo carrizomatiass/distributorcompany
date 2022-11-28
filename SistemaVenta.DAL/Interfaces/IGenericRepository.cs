93d9c741c3814dee8b1861023351a53489245ee4
carrizomatiass <carrizomatias007@gmail.com>
2022-11-28 15:43:16 -0300

    Entidades, obtener, crear, editar, eliminar

---
+++ SistemaVenta.DAL/Interfaces/IGenericRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace SistemaVenta.DAL.Interfaces
+{
+    public interface IGenericRepository <TEntity> where TEntity : class
+    {
+        Task<TEntity> ObtenerAsync (Expression<Func<TEntity, bool>> filtro);
+
+        Task<TEntity> CrearAsync(TEntity entidad);
+
+        Task<bool> EditarAsync(TEntity entidad);
+
+        Task<bool> EliminarAsync(TEntity entidad);
+
+        Task<IQueryable<TEntity>> Consultar(Expression<Func<TEntity, bool>> filtro= null);
+
+    }
+}
